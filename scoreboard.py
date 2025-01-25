c39cf828d9bc9ef9b260b6c635eb48a92b041721
araven0845 <aravendran05@gmail.com>
2025-01-24 17:22:51 -0800

    update file reading

--- scoreboard.py
+++ scoreboard.py
@@ -5,10 +5,11 @@ class Scoreboard(Turtle):
         # Initialize the scoreboard at the top of the screen
         super().__init__()
         self.score = 0
+        with open("data.txt") as f:
+            self.high_score = int(f.read())
         self.pu()
         self.color("white")
         self.goto(0, 260)
-        self.high_score = 0
         self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Courier", 24, "normal"))
         self.hideturtle()
 
@@ -20,8 +21,11 @@ class Scoreboard(Turtle):
         self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Courier", 24, "normal"))
 
     def reset_scoreboard(self):
+
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
 
         self.score = 0
         self.update()
